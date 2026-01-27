8be7688943c08ea5f9c40dffb8443d05825e90b3
AxelLCV <81644463+AxelLCV@users.noreply.github.com>
2026-01-27 22:21:09 +0100

    Delete dist directory

--- dist/routes/v1/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { validateRequest } from "../../middlewares/index.js";
-import { registerSchema, loginSchema } from "../../validators/index.js";
-import { authController } from "../../controllers/auth.controller.js";
-const router = Router();
-router.post("/register", validateRequest(registerSchema), authController.register);
-router.post("/login", validateRequest(loginSchema), authController.login);
-export default router;
