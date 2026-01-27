91cc7629378adfae89c522de8b44963448db385c
AxelLCV <axel.lecleve@gmail.com>
2026-01-27 23:21:21 +0100

    update package.json

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -7,4 +7,8 @@ const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
+prisma.$connect()
+  .then(() => console.log('✅ Database connected'))
+  .catch((err) => console.error('❌ Database connection error:', err));
+
 export { prisma }
