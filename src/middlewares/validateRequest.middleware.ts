12d0cbd0cb0e110a4f08b766971f33853dffae4f
AxelLCV <axel.lecleve@gmail.com>
2026-01-27 23:24:14 +0100

    update package.json

--- src/middlewares/validateRequest.middleware.ts
+++ src/middlewares/validateRequest.middleware.ts
@@ -21,6 +21,7 @@ export interface ValidatedRequest extends Request {
 // Middleware générateur
 export const validateRequest = (schema: RequestSchema) => {
   return (req: ValidatedRequest, res: Response, next: NextFunction) => {
+    console.log('test');
     const validated: { body?: any; params?: any; query?: any } = {};
 
     try {
