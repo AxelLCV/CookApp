92e07c186573a70c0c99daa86fcd23db4fd8cf72
AxelLCV <axel.lecleve@gmail.com>
2026-01-27 23:18:36 +0100

    update package.json

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(logger);
+app.get('/health', (req, res) => {
+  res.json({ status: 'ok' });
+});
 
 //Authentification routes
 app.use("/auth",authRoutes);
