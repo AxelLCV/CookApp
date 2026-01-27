1473e7f77fc81a238486055ad1763d5b654d1b18
AxelLCV <axel.lecleve@gmail.com>
2026-01-27 23:28:20 +0100

    update package.json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 // src/server.ts
 import app from "./app.js"; // app.ts exporté en ES Module
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '8080', 10);
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
