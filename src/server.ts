fcfc82772359eb77c6a009f76ebfb334ce577497
AxelLCV <axel.lecleve@gmail.com>
2026-01-27 23:39:47 +0100

    update package.json

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import app from "./app.js"; // app.ts exporté en ES Module
 
 const PORT = parseInt(process.env.PORT || '8080', 10);
-
+console.log('PORT from Railway:', process.env.PORT);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
